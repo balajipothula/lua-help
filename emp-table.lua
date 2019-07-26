7e6644211d39758e509f68b820977a6f1fb7c71f
BALAJI POTHULA <balan.pothula@gmail.com>
2019-07-26 10:37:51 +0530

    Lua help

--- emp-table.lua
+++ emp-table.lua
@@ -1,8 +1,8 @@
 local emp_table = { { no = "101", name = "Ram" },
                     { no = "102", name = "Sam" },
                     { no = "103", name = "Dev" },
-                    { no = "104", name = "Raj" } }
-
+                    { no = "104", name = "Tej" } }
+                  
 for _, emp in pairs(emp_table) do
   print(emp.no, emp.name)
 end
@@ -21,8 +21,17 @@ for i in ipairs(emp_table) do
   end  
 end
 
+for i = 1, #emp_table, 2 do
+  print(emp_table[i]["no"], emp_table[i]["name"])
+end
+
+for i = 2, #emp_table, 2 do
+  print(emp_table[i]["no"], emp_table[i]["name"])
+end
+
 print(emp_table[1]["no"], emp_table[1]["name"])
 
 print(emp_table[#emp_table]["no"], emp_table[#emp_table]["name"])
 
 print(emp_table[table.getn(emp_table)]["no"], emp_table[table.getn(emp_table)]["name"])
+
--- select.lua
+++ select.lua
@@ -5,3 +5,12 @@ print(select(2, string.find("BALAJI RAO POTHULA", "RAO"))) --> 10
 
 local i, e = select(1, string.find("BALAJI RAO POTHULA", "RAO"))
 print(i, e)
+
+
+function b(...)
+  for i = 1, select('#', ...) do
+    myTable[#myTable+1] = select(i,...)
+  end
+end
+
+b('c','d')
