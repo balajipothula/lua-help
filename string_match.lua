29554399c4f40ae934322c0f79b162dd876028bb
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-07 14:36:09 +0530

    Create string_match.lua

---
+++ string_match.lua
@@ -0,0 +1,4 @@
+print(string.match("emp id is 122692", '%d+'))
+print(string.match("emp id is 122692", '%d%d'))
+print(string.match("emp name is BALAJI", '%u+'))
+print(string.match("0123456789", '[4-7]+'))
