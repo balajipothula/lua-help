4a815e3cd6bb5742444293f67693f082fb1a04b9
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-08 14:52:00 +0530

    Update string_title_case.lua

--- string_title_case.lua
+++ string_title_case.lua
@@ -1,17 +1,17 @@
 local header_name = "X-Custom-Header"
 print(header_name)
 
-header_name = string.gsub(header_name, "-", "_")
+header_name = string.gsub(header_name, '-', '_')
 print(header_name)
 
 header_name = string.lower(header_name)
 print(header_name)
 
-header_name = string.gsub(header_name, "_", " ")
+header_name = string.gsub(header_name, '_', ' ')
 print(header_name)
 
-header_name = string.gsub(" " .. header_name, "%W%l", string.upper):sub(2)
+header_name = string.gsub(' ' .. header_name, "%W%l", string.upper):sub(2)
 print(header_name)
 
-header_name = string.gsub(header_name, " ", "-")
+header_name = string.gsub(header_name, ' ', '-')
 print(header_name)
